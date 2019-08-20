06f9e615a63ecaa280eadd3b64f8ce293bcef51a
zhangzhiqiang <1252320343@qq.com>
2019-08-21 00:56:08 +0800

    dev2  22

--- src/com/itheima/MyTest.java
+++ src/com/itheima/MyTest.java
@@ -2,8 +2,11 @@ package com.itheima;
 
 public class MyTest {
     public static void main(String[] args) {
+        System.out.println("123dev2");
         System.out.println("我是主分支");
         System.out.println("我是dev1分支");
         System.out.println("我是dev2分支");
     }
+
+
 }
