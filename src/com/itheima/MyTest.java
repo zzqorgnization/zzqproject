88e5f3754b8db72b1c418abad40bcbbed329b9fc
zhangzhiqiang <1252320343@qq.com>
2019-08-21 00:11:15 +0800

    我是主dev2 分支 的代码

--- src/com/itheima/MyTest.java
+++ src/com/itheima/MyTest.java
@@ -4,5 +4,6 @@ public class MyTest {
     public static void main(String[] args) {
         System.out.println("我是主分支");
         System.out.println("我是dev1分支");
+        System.out.println("我是dev2分支");
     }
 }
